7fe82e2775d3f01b65da952daf431278d798db05
nairamangasaryan <mangasaryannaira@gmail.com>
2023-03-10 23:34:38 +0400

    Includes examples of print() and input() functions

--- print_types/print_examples.py
+++ print_types/print_examples.py
@@ -2,9 +2,13 @@ print()
 x = "Anna", "Vardan", "Karen", "Gayane"
 print("New students are: ", x)
 print()
-name = input("Hi! What is your name? ", )
-print("I am very glad ", name)
-print()
 print('36 * 45 = ', 36*45)
 print()
+name = input("Hi! What is your name? ", )
+print("I am very glad ", name)
+number = input("How old are you? ", )
+print("You are ", number, "years old")
+print()
+
+
 
