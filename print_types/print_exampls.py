587d42ad0e2918e3261d5e0c791c24437ae4ff2c
nairamangasaryan <mangasaryannaira@gmail.com>
2023-03-10 23:06:06 +0400

    Some examples of print() function

---
+++ print_types/print_exampls.py
@@ -0,0 +1,10 @@
+print()
+x = "Anna", "Vardan", "Karen", "Gayane"
+print("New students are: ", x)
+print()
+name = input("Hi! What is your name? ", )
+print("I am very glad ", name)
+print()
+print('36 * 45 = ', 36*45)
+print()
+
