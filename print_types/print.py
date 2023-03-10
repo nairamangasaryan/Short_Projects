86a2e1a1adc3d9258e593d14c666b6042d7401c8
nairamangasaryan <mangasaryannaira@gmail.com>
2023-03-10 22:41:20 +0400

    Discription of 'print()' function

---
+++ print_types/print.py
@@ -0,0 +1,16 @@
+print()
+print("Good morning!")
+print('Good afternoon!')
+print("'Good evening!'")
+print()
+print("Good morning!\nGood afternoon!\n'Good evening!'")
+print()
+print("""
+    We can print:
+    - more than one object,
+    - strings,
+    - tuples
+    """)
+print("2+5=",2+5)
+
+
